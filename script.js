f10471928a8f8abf0160b06f464cf627a5b2e9d1
Alija Hodzic <hodzic.alija5@gmail.com>
2021-01-30 09:22:25 +0100

    Prvi commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let index=0;
+let banner = document.getElementById("text");
+let bannerText = "Loading your page..";
+let count = 0;
+let letter = "";
+let dot = document.getElementById("dot");
+let delay = bannerText.length * 135;
+
+function dotDisplay(){
+    dot.style.visibility = "visible";
+}
+
+setTimeout(dotDisplay,delay);
+
+
+(function typeEffect(){
+ 
+
+    if(index <= bannerText.length){
+      letter += bannerText[index];
+    }
+
+    banner.innerHTML = letter;
+    
+    index+=1
+    count+=1
+
+    console.log(letter)
+    let timeout = setTimeout(typeEffect, 120);
+   
+    if(count=== bannerText.length){
+        clearTimeout(timeout)
+    } 
+      
+    
+})();
+
+
+
+
+
+
+
+
